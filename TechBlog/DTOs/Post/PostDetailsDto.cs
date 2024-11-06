6b29f015db4be8606c4ddeec8753ba1b55223504
ppsasho <businessmail5545@gmail.com>
2024-11-06 20:30:51 +0100

    added Description to PostDetailsDto
    This new value won't be displayed, but instead will be used when updating a post on FrontEnd

--- TechBlog/DTOs/Post/PostDetailsDto.cs
+++ TechBlog/DTOs/Post/PostDetailsDto.cs
@@ -7,6 +7,7 @@ namespace DTOs.Post
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public string Text { get; set; }
         public UserDto User { get; set; }
         public decimal Rating { get; set; }
